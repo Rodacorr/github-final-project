69615df8a4a076fe2332d3cfcb4bbd69d78e26f8
Rodacorr <75710954+Rodacorr@users.noreply.github.com>
2023-06-12 12:04:07 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <Rodacorr>
    # Input:
    # p, principal amount
    # t, time period in years
